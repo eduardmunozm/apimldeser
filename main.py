0f18a6ddeaaacdb604a9816122587b6bf76cbfad
eduardmunozm <75817302+eduardmunozm@users.noreply.github.com>
2022-12-03 14:07:02 -0300

    Update main.py

--- main.py
+++ main.py
@@ -15,9 +15,7 @@ import json
 
 app = FastAPI()
 
-
-
- origins = ["*"]
+origins = ["*"]
  app.add_middleware(CORSMiddleware,
                     allow_origins=origins,
                    allow_credentials=True,
